bf77373c8dae444a6f09802a385e828b65a08b3d
akil <akil@nanotek.info>
2017-07-26 16:18:15 +0100

    add few headers to make it compile

--- scopedatasource.cpp
+++ scopedatasource.cpp
@@ -1,5 +1,15 @@
 #include <QDebug>
 
+#include <linux/types.h>
+#include <linux/socket.h>
+#include <bits/sockaddr.h>
+
+#include <netlink/socket.h>
+#include <netlink/errno.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/family.h>
+#include <netlink/genl/ctrl.h>
+
 #include <netlink/genl/genl.h>
 #include <netlink/genl/family.h>
 #include <netlink/genl/ctrl.h>
